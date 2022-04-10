8b03135ac1f12afaa8a7c7da197232521c72a463
Thanh Dat <dothanhdat275@gmail.com>
2022-04-10 23:26:32 +0900

    lab2.4

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const http = require("http");
 const express = require("express");
 const app = express();
-app.use((req, res, next) => {
-  console.log("In the middleware");
+app.use("", (req, res, next) => {
+  console.log("this always runs!");
   next(); //allow the request to continune to the next middleware in line
 });
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
+  console.log("In the another middleware");
+  res.send("<h1>The 'Add Product'</h1>");
+});
+
+app.use("/", (req, res, next) => {
   console.log("In the another middleware");
   res.send("<h1>Hello from Express</h1>");
 });
-const server = http.createServer(app);
-server.listen(3000);
+app.listen(3000);
