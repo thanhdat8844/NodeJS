7efa28f4df5ccade368b1aaca5b62b67782e6628
Thanh Dat <dothanhdat275@gmail.com>
2022-04-10 22:54:52 +0900

    lab2.3

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log("In the another middleware");
-  //....
+  res.send("<h1>Hello from Express</h1>");
 });
 const server = http.createServer(app);
 server.listen(3000);
