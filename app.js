f2043f93a77f17a81eabd67182663bdbfce02511
Thanh Dat <dothanhdat275@gmail.com>
2022-04-14 21:58:22 +0900

    lab2.10

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
