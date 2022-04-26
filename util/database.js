74d2bb33adadfd642fd839e769e8d84d6e8e51cf
Thanh Dat <dothanhdat275@gmail.com>
2022-04-27 00:17:27 +0900

    Retrieving Data

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  database: "node-compelete",
+  database: "node-complete",
   password: "Anhk0biet",
 });
 
