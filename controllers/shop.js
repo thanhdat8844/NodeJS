62a5b6adbd02bbd5448607b097d5d4f4400d6132
Thanh Dat <dothanhdat275@gmail.com>
2022-04-27 00:59:11 +0900

    Fetch Products - Time to Practice

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,13 +2,15 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/product-list", {
-      prods: products,
-      pageTitle: "All Products",
-      path: "/products",
-    });
-  });
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render("shop/product-list", {
+        prods: rows,
+        pageTitle: "All Products",
+        path: "/products",
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getProduct = (req, res, next) => {
@@ -70,6 +72,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
     res.redirect("/cart");
   });
 };
+
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     path: "/orders",
