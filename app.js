7a70f1c7a7d17f06b70c92e27649a38783d0d7d1
Thanh Dat <dothanhdat275@gmail.com>
2022-05-02 11:45:56 +0900

    Creating the Database Connection

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-// const adminRoutes = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 // const shopRoutes = require("./routes/shop");
 // const Order = require("./models/order");
 
